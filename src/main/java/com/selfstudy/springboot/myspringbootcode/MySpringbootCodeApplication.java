0afda8cb65de7735b049773a791e5a07422b3d03
kr2 <kamalendu@users.noreply.github.com>
2019-09-07 09:55:35 -0400

    init commit

---
+++ src/main/java/com/selfstudy/springboot/myspringbootcode/MySpringbootCodeApplication.java
@@ -0,0 +1,13 @@
+package com.selfstudy.springboot.myspringbootcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringbootCodeApplication.class, args);
+	}
+
+}
